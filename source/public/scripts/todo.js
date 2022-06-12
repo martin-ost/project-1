f6e3e7b1eac8f175d373d3bcd78dad44da635e23
“Martin <frym@bluewin.ch>
2022-06-12 08:52:28 +0200

    Updated ToDos

--- source/public/scripts/todo.js
+++ source/public/scripts/todo.js
@@ -1,21 +1,18 @@
 // TODO:
 // - Rework HTML/CSS, using Google guidelines.
 // - Check color schema for date picker, radio buttons and tick box.
-// - Implement info view.
-// - Keyboard navigation does not work for SVG "buttons"
+// - Implement info view, as modal window.
+// - Change style for keyboard navigation.
 // - How to locally install Handlebars, manually copy? Directly use NPM repository?
 // - Locally install fonts (.woff2).
-// - Rename item-list-controller to item-list-view.
 // - Dynamically generate selection lists (sort, filter) with Handlebars.
-// - What is teh problem with the function maps for filter and sort?
+// - What is the problem with the function maps for filter and sort?
 // - Test on different browsers/platforms.
 // - Configure ESLint.
 // - How to set tags in Github?
 // - Add class diagram to README.md.
-// - Confirmation box before deleting an item (as modal window).
 // - Check js code according to Google guidelines.
 // - How to define Handlebars in js code?
-// - Use "normalize.css" (or "reset.css", http://web.simmons.edu/~grovesd/comm244/notes/week4/css-reset).
 // - Rethink use validity for name label.
 
 import { TodoService } from './service/todo-service.js';
